1a973515825562beef3b026f17ed6be7ab88407b
Jurgden <82841924+jurgden@users.noreply.github.com>
2021-12-28 22:55:46 +0000

    Added Colons and a comma

--- main.py
+++ main.py
@@ -39,27 +39,27 @@ tickerDfT = tickerDataTSLA.history(period='1w', start='2011-12-21', end='2021-12
 
 
 st.write("# The FAANG Stock Spotlight")
-st.write("# F")
+st.write("# F:")
 st.write("# Facebook Stock Price History",
 "Facebook Closing stock and Volume history ")
 st.line_chart(tickerDfF.Close)
 st.line_chart(tickerDfF.Volume)
-st.write("# A")
+st.write("# A:")
 st.write("# Amazon Stock Price History",
 "Amazon Closing prices, followed by the Volume")
 st.line_chart(tickerDfA.Close)
 st.line_chart(tickerDfA.Volume)
-st.write("# A")
+st.write("# A:")
 st.write("# Apple Stock Price History",
 "Showcasing the Apple Closing and Volume graphed")
 st.line_chart(tickerDfA2.Close)
 st.line_chart(tickerDfA2.Volume)
-st.write("# N")
+st.write("# N:")
 st.write("# Netflix Stock History",
 "Netflix Closing prices, followed by the Volume")
 st.line_chart(tickerDfN.Close)
 st.line_chart(tickerDfN.Volume)
-st.write("# G")
+st.write("# G:")
 st.write("# Google Stock Price History",
 "The final FAANG stock on our list!")
 st.line_chart(tickerDfG.Close)
@@ -68,7 +68,7 @@ st.write("# ------------")
 st.write("# This is the whole FAANG Stock Closing/Volume Portfolio")
 st.write("Here is a bonus Tesla Tracker")
 
-st.write("# Tesla Stock Price History"
+st.write("# Tesla Stock Price History",
 "It's not FAANG but still cool! ")
 st.line_chart(tickerDfT.Close)
 st.line_chart(tickerDfT.Volume)
