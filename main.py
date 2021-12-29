022bc3a718aa7a383994a260f62cef1f87cfa2b2
Jurgden <82841924+jurgden@users.noreply.github.com>
2021-12-29 00:21:09 +0000

    more markdown

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 st.write("""
-### Tech Stock Price Tracking App
+# Tech Stock Price Tracking App
 
 Here are the ***FAANG*** **Closing** stock prices and their **Volumes** repectively!
 
@@ -40,36 +40,36 @@ tickerDfT = tickerDataTSLA.history(period='1w', start='2011-12-21', end='2021-12
 
 st.write("# ---The ***FAANG*** Stock Spotlight---")
 st.write("# ***F***:")
-st.write("# Facebook Stock Price History,",
+st.write("## Facebook Stock Price History,",
 "Facebook Closing stock and Volume history ")
 st.line_chart(tickerDfF.Close)
 st.line_chart(tickerDfF.Volume)
 st.write("# ***A***:")
-st.write("# Amazon Stock Price History,",
+st.write("## Amazon Stock Price History,",
 "Amazon Closing prices, followed by the Volume")
 st.line_chart(tickerDfA.Close)
 st.line_chart(tickerDfA.Volume)
 st.write("# ***A***:")
-st.write("# Apple Stock Price History,",
+st.write("## Apple Stock Price History,",
 "Showcasing the Apple Closing and Volume graphed")
 st.line_chart(tickerDfA2.Close)
 st.line_chart(tickerDfA2.Volume)
 st.write("# ***N***:")
-st.write("# Netflix Stock History,",
+st.write("## Netflix Stock History,",
 "Netflix Closing prices followed by the Volume")
 st.line_chart(tickerDfN.Close)
 st.line_chart(tickerDfN.Volume)
 st.write("# ***G***:")
-st.write("# Google Stock Price History,",
+st.write("## Google Stock Price History,",
 "The final FAANG stock on our list!")
 st.line_chart(tickerDfG.Close)
 st.line_chart(tickerDfG.Volume)
-st.write("# -------------------------------------------")
+st.write("## -------------------------------------------")
 st.write("# This is the whole ***FAANG*** Stock Closing/Volume Portfolio")
-st.write("# -------------------------------------------")
+st.write("## -------------------------------------------")
 st.write("Here is a bonus Tesla Tracker")
 
-st.write("# Tesla Stock Price History,",
+st.write("## Tesla Stock Price History,",
 "It's not ***FAANG*** but still cool! ")
 st.line_chart(tickerDfT.Close)
 st.line_chart(tickerDfT.Volume)
